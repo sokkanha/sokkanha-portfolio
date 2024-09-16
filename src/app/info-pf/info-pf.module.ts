ae482dc8371c4570d40af0bbf4f2f7a9ce46cc3e
sokkanha <121841538+sokkanha@users.noreply.github.com>
2024-09-16 11:34:18 +0700

    custom Info part

--- src/app/info-pf/info-pf.module.ts
+++ src/app/info-pf/info-pf.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InfoComponent } from './info/info.component';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
 @NgModule({
   declarations: [InfoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
   exports: [InfoComponent]
 })
